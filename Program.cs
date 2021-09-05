41f213521fb87c7988527a4c958533e9c9ba590c
Josh Barton <joshdbarton@gmail.com>
2021-09-05 14:42:39 -0500

    adding another instantiation example

--- Program.cs
+++ Program.cs
@@ -23,6 +23,18 @@ namespace Inheritance
 
             animals[0].Sleep();
             Console.WriteLine($"{animals[0].Name} is {animals[0].SleepStatus}");
+
+            Dog spot = new Dog
+            {
+                Name = "Spot",
+                FurLength = 15
+            };
+            spot.Sleep();
+            Console.Write($"{spot.Name} is ");
+            Console.Write(spot.SleepStatus);
+            //output: Spot is asleep
+            spot.Speak();
+            //output: "woof!"
         }
 
 
